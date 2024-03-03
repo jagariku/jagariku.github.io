df78eb62f2571d5a5ac41e44d72249440c475c24
riku <riku@example.com>
2024-03-04 00:00:19 +0900

    astroを導入してみる

---
+++ awesome-altitude/astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+
+export default defineConfig({
+  integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+  buildOptions: {
+    outDir: "../../",
+  },
+});
